556717d88f571bfb489ca5d36ee082084db27b20
Atef Lassoued <ateflassoued2016@gmail.com>
2021-05-11 03:27:42 +0000

    Delete Music Website directory

--- Music Website/js/script.js
+++
@@ -1,9 +0,0 @@
-var mySong = document.getElementById("mySong");
-var play = document.getElementById("icon1");
-var pause = document.getElementById("icon2");
-play.onclick = function () {
-    mySong.play ();
-}
-pause.onclick = function () {
-    mySong.pause ();
-}
